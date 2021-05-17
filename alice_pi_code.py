0a0092ab872e413bc4ce3402c7534c974e4a15d9
aivanits <62224965+aivanits@users.noreply.github.com>
2021-05-16 22:47:35 -0400

    misc

---
+++ alice_pi_code.py
@@ -0,0 +1,17 @@
+
+# timed loop (restarts loop after x amount of seconds )
+
+#things to get pi signal 
+
+#connect to api stuff???S
+
+
+float signal = photoVal 
+
+
+if pi signal > some photoval ( idk how this sends )
+    PUT/state = true
+    # sends boolean of true: sign is on 
+else if
+    PUT/state = false
+    #  sends boolean of false: sign is off 
